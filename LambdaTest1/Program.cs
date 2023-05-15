679bbcabdf01ea206ad0e8a9df17e8f37311903e
Krzysztof Szwajka <krzysztof.szwajka@student.wsg.pl>
2023-05-15 12:06:27 +0200

    Add project files.

---
+++ LambdaTest1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+/// <summary>
+/// wyrazenie lambda
+/// </summary>
+var show = (string a) => { Console.WriteLine(a); }; // wyrazenie lambda funkcja anonimowa wypisujaca tekst
+var iloczyn = (int x, int y) => { return x * y; };  // wyraenie lambda funkcja anoniowa iloczyn dwow liczb
+show("jakis string");  // wywolanie funkcji anonimowej tekst, jako parametry staly tekst
+show(iloczyn(2,3).ToString()); // wywolanie funkcji anonimowej teks, jako parametr funkcja anonimowa iloczyn
+                                // wartosc zracana jest zamianiana na teks
+
